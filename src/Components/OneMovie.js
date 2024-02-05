386e4356b87535f1ef0e5df5a2d1d07bb56e6fbf
Your Name <Your email>
2024-02-05 23:22:29 +0100

    new update wiouu

--- src/Components/OneMovie.js
+++ src/Components/OneMovie.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-const OneMovie = ({getDataFrom}) => {
+const OneMovie = ({ getDataFrom }) => {
   const para = useParams();
-  const OneMov= getDataFrom.filter((film)=> film.id == para.id)[0]; 
-  console.log(OneMov)
-  console.log()
-  return (
-    <div>
-    <h1>{OneMov.title}</h1> 
-    <img src={OneMov.image} alt="" />
-    <p>{OneMov.decr}</p> 
-    <h4>{OneMov.stream}</h4> 
-    
-    </div>
-  )
-}
+  const OneMov = getDataFrom.filter((film) => film.id == para.id)[0];
+  console.log(OneMov);
 
-export default OneMovie
+  if (!OneMov) {
+    return (
+      <div>
+        <h1>{OneMov.title}</h1>
+        <img src={OneMov.image} alt="" />
+        <p>{OneMov.decr}</p>
+        <h4>{OneMov.genre}</h4>
+        <h3>lang:{OneMov.lang}</h3>
+        <video src={OneMov.stream} controls />
+        <button>
+          <a href="https://filelions.live/d/yfeamhvozrrf">Downlaod</a>
+        </button>
+      </div>
+    );
+  } else {
+    return <div>no</div>;
+  }
+};
+
+export default OneMovie;
