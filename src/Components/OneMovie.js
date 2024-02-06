490a4c1f8704b3cdd3958f37b61ab9db292a3952
Your Name <Your email>
2024-02-06 16:03:08 +0100

    last upDate

--- src/Components/OneMovie.js
+++ src/Components/OneMovie.js
@@ -1,27 +1,39 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "../CSS/OneMovie.css";
 
 const OneMovie = ({ getDataFrom }) => {
   const para = useParams();
   const OneMov = getDataFrom.filter((film) => film.id == para.id)[0];
   console.log(OneMov);
 
-  if (!OneMov) {
+  if (OneMov) {
     return (
-      <div>
-        <h1>{OneMov.title}</h1>
-        <img src={OneMov.image} alt="" />
-        <p>{OneMov.decr}</p>
-        <h4>{OneMov.genre}</h4>
-        <h3>lang:{OneMov.lang}</h3>
-        <video src={OneMov.stream} controls />
-        <button>
-          <a href="https://filelions.live/d/yfeamhvozrrf">Downlaod</a>
-        </button>
+      <div className="global">
+        <div className="overaly">
+          <h1>{OneMov.title}</h1>
+          <div className="overaly-into">
+            <img src={OneMov.image} alt="" />
+            <div className="info">
+              <label> Description :</label>
+              <p>{OneMov.decr}</p>
+              <label>Genre :</label>
+              <p>{OneMov.genre}</p>
+              <label>Lang :</label>
+              <p>{OneMov.lang}</p>
+              <label>Rate :</label>
+              <p>{OneMov.rate} 
+              <i class="fa-solid fa-star"/> 
+              </p>
+            </div>
+          </div>
+          <video src={OneMov.stream} controls />
+          <button>
+            <a href="https://filelions.live/d/yfeamhvozrrf">Downlaod</a>
+          </button>
+        </div>
       </div>
     );
-  } else {
-    return <div>no</div>;
   }
 };
 
